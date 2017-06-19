c59049ce325740bbbbfbfaecac30ae03a91419b5
Andrew Laffoon <andrew@mixbook.com>
2017-06-19 00:29:57 -0700

    Update action.py

--- src/action.py
+++ src/action.py
@@ -327,6 +327,7 @@ class PlayFart(object):
         num_farts = len(fart_sounds)
         which_fart = random.randint(0, num_farts-1)
         
+        self.say('playing fart ' + which_fart)
         PlaySound(fart_sounds[which_fart])
         
         
